a7774cf4fcd0c921c28add7eb8b727620696c3ae
John Osterman <ostermanj@gmail.com>
2018-09-11 22:33:24 -0400

    Footer changes

--- cdi2016_footer.tpl.php
+++ cdi2016_footer.tpl.php
@@ -6,16 +6,16 @@
             <div class="description"><?php print t('This year\'s brief');?></div>
             <a href="#" class="cdi-footer-link"></a>
         </div>
-        <div class="footer-block data" onclick="window.open('https://drive.google.com/drive/folders/0B167nw-gSVHRcXhILXg0RjM3Vmc')">
+        <div class="footer-block indicators" onclick="window.open('https://drive.google.com/drive/folders/0B167nw-gSVHRcXhILXg0RjM3Vmc')">
             <div class="cdi-icon"></div>
             <div class="title"><?php print t('Explore');?></div>
-            <div class="description"><?php print t('The data and methodology');?></div>
+            <div class="description"><?php print t('The indicators and methodology');?></div>
             <a href="#" class="cdi-footer-link"></a>
         </div>
-        <div class="footer-block video" onclick="window.open('//www.youtube.com/watch?v=_e6TTZYomTk')">
+        <div class="footer-block data" onclick="window.open('//www.youtube.com/watch?v=_e6TTZYomTk')">
             <div class="cdi-icon"></div>
-            <div class="title"><?php print t('Watch');?></div>
-            <div class="description"><?php print t('A video teaser about the CDI');?></div>
+            <div class="title"><?php print t('Examine');?></div>
+            <div class="description"><?php print t('The data');?></div>
             <a href="#" class="cdi-footer-link"></a>
         </div>
         <div class="footer-block membership" onclick="window.open('//www.cgdev.org/page/cdi-consortium')">
